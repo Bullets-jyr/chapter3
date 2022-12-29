0dbaaed2243d0ce01489427eb0dbe629f9189904
Bullets-jyr <jyr@bullets.co.kr>
2022-12-29 20:22:54 +0900

    04. UI 요소 가져오기

--- app/src/main/java/kr/co/bullets/chapater3/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapater3/MainActivity.kt
@@ -2,10 +2,20 @@ package kr.co.bullets.chapater3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.bullets.chapater3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditTextView = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
     }
 }
